1dbcb25cf673538fc11a34713c1230833de63314
Henry Chan <79147973+henryphchan@users.noreply.github.com>
2022-07-01 12:27:05 +0800

    Create 392_is-subsequence.py

---
+++ 392_is-subsequence.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        ptrS = 0
+        ptrT = 0
+        while ptrS < len(s) and ptrT < len(t):
+            if s[ptrS] == t[ptrT]:
+                ptrS += 1
+            ptrT += 1
+        if ptrS >= len(s):
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isSubsequence("abc","ahbgdc"))
+    pritn(s.isSubsequence("axc","ahbgdc"))
