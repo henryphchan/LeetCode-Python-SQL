81b06e940165aaea364b4649311e5ec97523d98e
Henry Chan <79147973+henryphchan@users.noreply.github.com>
2022-08-02 23:03:46 -0700

    Create 88_merge-sorted-array.py

---
+++ 88_merge-sorted-array.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/merge-sorted-array/
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m-1
+        p2 = n-1
+        for p in range(m+n-1,-1,-1):
+            if p2 < 0:
+                return
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+
+if __name__ == '__main__':
+    s = Solution()
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    nums2 = [2,5,6]
+    n = 3
+    s.merge(nums1,m,nums2,n)
+    print(nums1)
