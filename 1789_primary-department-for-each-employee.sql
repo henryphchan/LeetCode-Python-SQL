2729359b3b7ee9c198930064a45f2b181c0923be
Henry Chan <79147973+henryphchan@users.noreply.github.com>
2023-06-23 00:16:37 -0700

    Update 1789_primary-department-for-each-employee.sql

---
+++ 1789_primary-department-for-each-employee.sql
@@ -0,0 +1,12 @@
+/* https://leetcode.com/problems/primary-department-for-each-employee */
+/* Write your T-SQL query statement below */
+select employee_id, department_id
+from employee
+where primary_flag = 'Y'
+union
+select employee_id, department_id
+from employee
+where employee_id in (select employee_id
+from employee
+group by employee_id
+having count(*) = 1)
